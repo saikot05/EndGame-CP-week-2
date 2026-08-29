47c17a40c5551abe8e67aa4b2ba3174242799ced
saikot05 <saikotislam08@gmail.com>
2026-08-29 23:32:00 +0600

    solve findmax problem

--- EG-13_Sprint-2_PS.js
+++ EG-13_Sprint-2_PS.js
@@ -6,3 +6,20 @@
 function reverseString(str) {
     return str.split('').reverse().join('');
 }
+//2. Find Maximum
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function findMax(nums) {
+    if (nums.length === 0) {
+        return undefined;
+    }
+    let mx = nums[0];
+    for (const val of nums) {
+        if (val > mx) {
+            mx = val;
+        }
+    }
+    return mx;
+}
